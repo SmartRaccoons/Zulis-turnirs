81302d3fed7d962ca3517d756647811429407092
Valuks <termilv@gmail.com>
2016-02-16 12:13:10 +0200

    change fonts

--- d/javascripts/poster.js
+++ d/javascripts/poster.js
@@ -7,19 +7,20 @@
     };
     var render = function () {
         var stage = new PIXI.Stage(0xFFFFFF);
+        var poster_type = $('input[name="poster-type"]:checked').val();
+        var poster_out = poster === 'out';
         (function () {
-            mid = new PIXI.Sprite(PIXI.Texture.fromImage('d/images/poster-back-'+$('input[name="poster-type"]:checked').val()+'.png'));
+            mid = new PIXI.Sprite(PIXI.Texture.fromImage('d/images/poster-back-'+poster_type+'.png?'));
             mid.position.x = 0;
             mid.position.y = 0;
             stage.addChild(mid);
         })();
         var t = function (t, size, color, position, center) {
             var richText = new PIXI.Text(t, {
-                font: 'normal ' + size + 'px hero',
+                font: 'normal ' + size + 'px gothic',
                 fill: color,
                 align: center === true ? 'center' : 'left'
             });
-
             richText.position.x = position[0];
             richText.position.y = position[1];
             if (center === true) {
@@ -29,7 +30,8 @@
         };
 
         t(v('day'), 80, '#b5b5b5', [60, 50]);
-        t(v('when'), 140, '#ff6c00', [60, 120]);
+        t(v('when'), 140, '#ff6c00', [50, 120]);
+        t(v('when-time'), 90, poster_out ? '#000000' : '#656565', [60, 250]);
         t(v('which'), 150, '#ff6c00', [620, 980], true);
         t(v('where'), 170, '#ff6c00', [620, 1150], true);
 
