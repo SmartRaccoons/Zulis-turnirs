eb092f27e8df4832120183ff7f8dbfd99c7fe808
Valuks <termilv@gmail.com>
2018-03-01 14:04:18 +0200

    poster full params

--- d/javascripts/poster.js
+++ d/javascripts/poster.js
@@ -10,16 +10,18 @@
         var stage = new PIXI.Stage(0xFFFFFF);
         var poster_type = $('input[name="poster-type"]:checked').val();
         var poster_out = poster_type === 'out';
-        var t = function (t, size, color, position, center) {
+        var t = function (t, size, color, position, align, font) {
             var richText = new PIXI.Text(t, {
-                font: 'normal ' + size + 'px gothic',
+                font: 'normal ' + size + 'px ' + (!font ? 'gothic' : font),
                 fill: color,
-                align: center === true ? 'center' : 'left'
+                align: align ? align : 'left'
             });
             richText.position.x = position[0];
             richText.position.y = position[1];
-            if (center === true) {
+            if (align === 'center') {
                 richText.anchor.x = 0.5;
+            } else if (align === 'right') {
+              richText.anchor.x = 1
             }
             stage.addChild(richText);
         };
@@ -30,13 +32,18 @@
             stage.addChild(i);
         };
 
-        img('d/images/poster-back-'+poster_type+'.png?' + _random, [0, 0])
+        img('d/images/back-'+poster_type+'.jpg?' + _random, [0, 0])
 
         t(v('day'), 80, '#b5b5b5', [60, 50]);
         t(v('when'), 140, '#ff6c00', [50, 120]);
         t(v('when-time'), 90, poster_out ? '#000000' : '#656565', [60, 250]);
-        t(v('which'), 150, '#ff6c00', [620, 980], true);
-        t(v('where'), 170, '#ff6c00', [620, 1150], true);
+        t(v('organize-text'), 60, poster_out ? '#000000' : '#656565', [1200, 50], 'right', 'hero');
+        t(v('what'), 450, poster_out ? '#000000' : '#d3d3d3', [620, 400], 'center', 'hero');
+        t(v('what2'), 200, poster_out ? '#000000' : '#d3d3d3', [620, 800], 'center', 'hero');
+        t(v('which'), 100, '#ff6c00', [620, 980], 'center');
+        t(v('where'), 180, '#ff6c00', [620, 1080], 'center');
+        t(v('where2'), 70, '#ff6c00', [620, 1280], 'center');
+        t(v('info'), 50, '#ff6c00', [620, 1400], 'center');
         t(v('submit'), 35, poster_out ? '#000000' : '#ffffff', [60, 1560]);
         t(v('submit-web'), 55, poster_out ? '#000000' : '#ffffff', [60, 1600]);
         t(v('submit-mob'), 55, poster_out ? '#000000' : '#ffffff', [60, 1660]);
