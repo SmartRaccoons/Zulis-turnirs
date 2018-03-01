8f58aa333099345a60acbee321550ed8a4f4c8fe
Valuks <termilv@gmail.com>
2018-03-01 12:22:49 +0200

    remove cache

--- d/javascripts/poster.js
+++ d/javascripts/poster.js
@@ -5,16 +5,11 @@
     var v = function (v) {
         return document.getElementById('poster-' + v).value;
     };
+    var _random = Math.random();
     var render = function () {
         var stage = new PIXI.Stage(0xFFFFFF);
         var poster_type = $('input[name="poster-type"]:checked').val();
         var poster_out = poster_type === 'out';
-        (function () {
-            mid = new PIXI.Sprite(PIXI.Texture.fromImage('d/images/poster-back-'+poster_type+'.png?'));
-            mid.position.x = 0;
-            mid.position.y = 0;
-            stage.addChild(mid);
-        })();
         var t = function (t, size, color, position, center) {
             var richText = new PIXI.Text(t, {
                 font: 'normal ' + size + 'px gothic',
@@ -29,11 +24,13 @@
             stage.addChild(richText);
         };
         var img = function (src, position) {
-            img = new PIXI.Sprite(PIXI.Texture.fromImage(src));
-            img.position.x = position[0];
-            img.position.y = position[1];
-            stage.addChild(img);
+            i = new PIXI.Sprite(PIXI.Texture.fromImage(src));
+            i.position.x = position[0];
+            i.position.y = position[1];
+            stage.addChild(i);
         };
+
+        img('d/images/poster-back-'+poster_type+'.png?' + _random, [0, 0])
 
         t(v('day'), 80, '#b5b5b5', [60, 50]);
         t(v('when'), 140, '#ff6c00', [50, 120]);
